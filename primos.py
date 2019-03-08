6d36ed770e665d4eeaa1f1f312c0e871b696c31d
Ramirez Tamayo <000404642@bga.upb>
2019-03-08 12:44:06 -0500

    Mi primer commit

---
+++ primos.py
@@ -0,0 +1,6 @@
+a=int(input("Digite el número que va ser divido"))
+b=int(input("Digite el número divisor"))
+if a%b==0:
+    print("Si es divisible exacto")
+else:
+    print("No es divisible exacto")
