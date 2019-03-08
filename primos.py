264f1b416006d4afec6bded7fc1577ad9bd4454f
Ramirez Tamayo <000404642@bga.upb>
2019-03-08 13:59:13 -0500

    Ejercicio resuelto

--- primos.py
+++ primos.py
@@ -1,7 +1,14 @@
-cont=0
-a=int(input("Digite un número"))
-for i in range (1,a):
-  residuo=a%i
-  print (residuo)
-  cont=cont+1
+cont = 0
+a = int(input("Digite un número!"))
+
+for i in range(1, a + 1):
+
+    if a % i == 0:
+        cont = cont + 1
+
+if cont > 2:
+    print("El número no es primo")
+else:
+    print("El número es primo")
+
 
